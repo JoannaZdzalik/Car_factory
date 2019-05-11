fbe26c4a97630a6132bdff2fb6c778e9a342c362
Joanna Zdzalik <asia.zdzalik@gmail.com>
2019-05-11 13:56:08 +0200

    added class car

---
+++ src/main/java/carfactory/Car.java
@@ -0,0 +1,14 @@
+package carfactory;
+
+public class Car {
+
+    private String number;
+    private int price;
+    private String owner;
+
+    public Car(String number, int price, String owner) {
+        this.number = number;
+        this.price = price;
+        this.owner = owner;
+    }
+}
